6f66c48e792cd68ad8ec47a541f19c4f81403307
melo_gustavo <gustavomelo95@gmail.com>
2020-08-23 10:03:26 -0300

    Create user migration and model

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,11 +3,11 @@ import Sequelize from 'sequelize';
 
 import databaseConfig from '../config/database';
 
-
+import User from '../api/models/User';
 
 
 const models = [
-
+  User
 ];
 
 class DataBase {
